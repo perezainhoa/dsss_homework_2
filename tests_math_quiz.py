9e4cdbb6d6a5398d01fa3b399439725cbaff08f9
perezainhoa <ainhoa9.ap@gmail.com>
2023-11-12 22:08:29 +0100

    tests added to tests_math_quiz.py

--- tests_math_quiz.py
+++ tests_math_quiz.py
@@ -1,30 +1,41 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import random_integer_generator, random_operator_selection, mathematical_operation
 
 
 class TestMathGame(unittest.TestCase):
 
-    def test_function_A(self):
+    def test_random_integer_generator(self):
         # Test if random numbers generated are within the specified range
-        min_val = 1
-        max_val = 10
+        min_value = 1
+        max_value = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
-            self.assertTrue(min_val <= rand_num <= max_val)
+            rand_num = random_integer_generator(min_value, max_value)
+            self.assertTrue(min_value <= rand_num <= max_value)
 
-    def test_function_B(self):
-        # TODO
-        pass
+    def test_random_operator_selection(self):
+        # Test if the generated random operator is '+', '-' or '*'
+        possible_operators = {'+','-','*'}
+        for _ in range(1000):  # Test a large number of random values
+            rand_ope = random_operator_selection()
+            self.assertIn(rand_ope, possible_operators)
 
-    def test_function_C(self):
+    def test_mathematical_operation(self):
             test_cases = [
                 (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
+                (4, 6, '+', '4 + 6', 10),
+                (1, 8, '+', '1 + 8', 9),
+                (5, 2, '-', '5 - 2', 3),
+                (4, 6, '-', '4 - 6', -2),
+                (1, 8, '-', '1 - 8', -7),
+                (5, 2, '*', '5 * 2', 10),
+                (4, 6, '*', '4 * 6', 24),
+                (1, 8, '*', '1 * 8', 8),
             ]
 
             for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+                operation, result = mathematical_operation(num1,num2,operator)
+                self.assertEqual(operation, expected_problem)
+                self.assertEqual(result,expected_answer)
 
 if __name__ == "__main__":
     unittest.main()
