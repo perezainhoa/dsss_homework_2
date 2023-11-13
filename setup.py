374764d11007943c7d115ab62d49cb4557743c38
perezainhoa <ainhoa9.ap@gmail.com>
2023-11-13 14:10:52 +0100

    Correction of setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='dsss_homework_2',
+    version='2.0',
+    author='perezainhoa',
+    description='DSSS homework2 - Setting up a git repository, cleaning and improving the provided code and making the repository pip installable',
+    url='https://github.com/perezainhoa/dsss_homework_2.git',
+    packages=['math_quiz'],
+    license='MIT'
+)
